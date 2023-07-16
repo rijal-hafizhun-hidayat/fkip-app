e296f22bfeec1a81985a95bd371b14817a80ef7c
unknown <rijal.1344@gmail.com>
2023-07-16 14:09:07 +0700

    edit class name

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -66,7 +66,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'isAdmin' => \App\Http\Middleware\IsAdmin::class,
-        'isAdminGuruPamong' => \App\Http\Middleware\isAdminGuruPamong::class,
+        'isAdminGuruPamong' => \App\Http\Middleware\IsAdminGuruPamong::class,
         'isAdminDpl' => \App\Http\Middleware\IsAdminDpl::class,
         'isGuruPamong' => \App\Http\Middleware\IsGuruPamong::class,
     ];
